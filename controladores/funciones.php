fd252eebd59e160aa9ab2910732997648d518091
David <erik.system32>
2020-04-18 12:40:19 -0600

    funcion de edad

--- controladores/funciones.php
+++ controladores/funciones.php
@@ -12,4 +12,26 @@ function generar_token(){
     }
     return time().$pass;
 }
+function mi_edad($fecha_nac){
+	//
+	$dia=date("j");
+	$mes=date("n");
+	$anno=date("Y");
+	//descomponer fecha de nacimiento
+	$anno_nac=substr($fecha_nac, 0, 4);
+	$mes_nac=substr($fecha_nac, 5, 2);
+	$dia_nac=substr($fecha_nac, 8, 2);
+	//
+	if($mes_nac>$mes){
+		$calc_edad= $anno-$anno_nac-1;
+	}else{
+		if($mes==$mes_nac AND $dia_nac>$dia){
+			$calc_edad= $anno-$anno_nac-1;  
+		}else{
+			$calc_edad= $anno-$anno_nac;
+		}
+	}
+    return $calc_edad;
+}
+?>
 ?>
